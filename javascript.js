fe5e9228c91ded026aa94faf1296aa4670f4f326
Juan Correa <juancorrea50@yahoo.com>
2023-08-21 18:21:55 -0500

    Tested and confirmed movement of private variable
    Create pipeline functions to transition player option to
    displayController() function.

--- javascript.js
+++ javascript.js
@@ -31,8 +31,7 @@ const createPlayerInfo =(function () {
     //Confirm button disabled by default
     _confrmChce.disabled=true;
     //variable to store player object
-    let playerOne = player('');
-    console.log(playerOne)
+    let _playerOne = player('');
 
     //Function for choice change
     function cacheAnswer(){
@@ -53,14 +52,13 @@ const createPlayerInfo =(function () {
         _dialogWindow.close(_selectEl.value);
         console.log('Player One Marker Chosen: ' + _confrmChce.value);
         _setPlayerMarker(_selectEl.value);
-        console.log('submit commenced');
     }
     function _setPlayerMarker(mrkr){
-        playerOne.marker = mrkr;
-        console.log(playerOne);
+        _playerOne.marker = mrkr;
+        console.log(_playerOne);
     }
     function getPlayerMarker() {
-        return playerOne.marker;
+        return _playerOne.marker;
     }
     
     _selectEl.addEventListener('change', cacheAnswer);
@@ -69,6 +67,7 @@ const createPlayerInfo =(function () {
         submitCheck(e);
         displayController.createNewGrid();
         displayController.fillGame();
+        displayController.savePlayerMark(getPlayerMarker());
     });
 
     //Return values when called
@@ -84,38 +83,46 @@ const displayController = (function () {
     //cacheDom(creates variables for queryselector DOM elements)
     const _gameContainer = document.querySelector('.game-container');
     const _gameBoardGrid = document.createElement('div');
+    let _playerMarker;
     _gameBoardGrid.classList.add('game-board-grid');
-    
     
     //Method to create new grid inside of the game container div
     function createNewGrid() {
         _gameContainer.appendChild(_gameBoardGrid);
     }
+    function savePlayerMark(mrkr){
+        _playerMarker = mrkr;
+        console.log(_playerMarker);
+    }
+    //Function to fill clicked div with the marker from the player
+    function markDiv(){
+
+    }
     //Fills the game with future interactive divs to fill them with the player marker
     function fillGame(){
-            const player = createPlayerInfo.getPlayerMarker();
-            console.log(player);
-            //Activate an onclick listener function that fills divs with the player marker
-            /*function answerClick(){
-                divs.innerText = player;
-            }*/
-            //Carry gameboard object here to access array.        
-            const _gameboardCall = gameBoard;
-            //Fills gameboard container with divs
-            for(let i =0; i<9; i++){
-                //create gameboard divs that will be referenced on click for player interaction
-                const divs = document.createElement('div');
-                divs.classList.add(`item-${i}`);
-                //Fills array with blank space for testing
-                gameBoardArray[i] = '';
-                divs.innerText = gameBoardArray[i];
-                _gameBoardGrid.appendChild(divs);
-                //Potentially fill this with the event listener 
-            }
+        //Carry gameboard object here to access array.        
+        const _gameboardCall = gameBoard;
+        //Fills gameboard container with divs
+        for(let i =0; i<9; i++){
+            //create gameboard divs that will be referenced on click for player interaction
+            const btns = document.createElement('button');
+            btns.classList.add(`item-${i}`);
+            //Fills array with blank space for testing
+            gameBoardArray[i] = '';
+            btns.innerText = gameBoardArray[i];
+            /*Save for when variable is carried
+            btns.addEventListener('click', () => {
+                console.log('button clicked Player Marker' + _playerMarker);
+                btns.innerText = _playerMarker;
+            });*/
+            _gameBoardGrid.appendChild(btns);
+            //Potentially fill this with the event listener
+            //Activate an onclick listener function that fills divs with the player marker(Continue from here)
+        }
             console.log(_gameboardCall)
     }
     
-    return {createNewGrid, fillGame};
+    return {createNewGrid, fillGame, savePlayerMark};
  
 })();
 
@@ -123,7 +130,6 @@ const displayController = (function () {
 const createGame =(function () {
        function init() {
         createPlayerInfo;
-        console.log(createPlayerInfo.getPlayerMarker());
     }
     init();
 
